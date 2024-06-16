a96e4030f90482e2d6600db434bf2c38942357d2
o1059de4649 <agi.13ms@gmail.com>
2024-06-16 17:27:33 +0900

    玉のぶつかる判定を実装

---
+++ Assets/Scripts/CombineBallController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombineBallController : MonoBehaviour
+{
+    /// <summary>
+    /// ボールのランク
+    /// </summary>
+    public int rank;
+    public BallMaster ballMaster;
+    public GameObject ballMasterObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //もし図鑑がないなら、とりにいく。
+        if (!ballMaster) {
+            ballMaster = GameObject.FindGameObjectWithTag("GameController").GetComponent<BallMaster>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// ぶつかると1回動く関数
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //ボールの情報が準備されていない場合は処理を終了する。
+        if (!ballMaster) return;
+        if (ballMaster.Balls.Count == 0) return;
+
+        // もし同じ種類のボールがぶつかったら、消える。
+        // 新しく、次のランクのボールが1つ増える。
+        if (collision.transform.gameObject.name.Contains(this.gameObject.name)) {
+
+            Debug.Log("ぶつかってきた方" + collision.transform.position.y);
+            Debug.Log("ぶつかられた方" + this.gameObject.transform.position.y);
+
+            //Y軸で片方を判定する(自分のほうが低い場合)
+            if (collision.transform.position.y >= this.gameObject.transform.position.y) {
+                
+                //次のランクレベル
+                int nextRank = rank + 1;
+
+                //★次のランクのボールがない場合は実行しない。
+                if (nextRank < ballMaster.Balls.Count)
+                {
+                    //次のランクのボールが1つ増える。
+                    GameObject nextBall = ballMaster.Balls[nextRank];
+                    var obj = Instantiate(nextBall, this.gameObject.transform.position, Quaternion.identity);
+                    //名前を同じ名前にする
+                    obj.name = obj.name.Replace("(Clone)", "");
+                }
+            }
+
+            //消える。
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerActionController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class PlayerActionController : MonoBehaviour
+{
+    private GameObject _playerBall;
+    public GameObject playerBall { 
+       get { return _playerBall; }
+       set {
+            _playerBall = value;
+            //画像差し替え
+            UpdatePlayerBallImage(_playerBall);
+        }
+    }
+    public BallMaster ballMaster;
+    public List<GameObject> playerBallImages = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBall = selectRandomBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左クリックで発射
+        if (Input.GetMouseButtonDown(0)) {
+            Shot(playerBall);
+        }
+    }
+
+    /// <summary>
+    /// ランダムなボールを返す。
+    /// </summary>
+    /// <returns></returns>
+    public GameObject selectRandomBall()
+    {
+        var balls = ballMaster.Balls;
+        var result = balls[Random.Range(0, balls.Count - 1)];
+        return result;
+    }
+
+    /// <summary>
+    /// ボールを解き放つ
+    /// </summary>
+    public void Shot(GameObject ball) {
+
+        // マウスのポインタがあるスクリーン座標を取得
+        Vector3 screen_point = Input.mousePosition;
+        // z に 1 を入れないと正しく変換できない
+        screen_point.z = 1.0f;
+        // スクリーン座標をワールド座標に変換
+        Vector3 world_position = Camera.main.ScreenToWorldPoint(screen_point);
+
+        //対象座標
+        var mousePosition = world_position;
+
+        //ボールを解き放つ
+        var obj = Instantiate(ball, mousePosition, Quaternion.identity);
+        obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, -5);
+        //名前を同じ名前にする
+        obj.name = obj.name.Replace("(Clone)", "");
+        //ボール再設定
+        playerBall = selectRandomBall();
+    }
+
+    /// <summary>
+    /// プレイヤーの持っているボールの画像
+    /// </summary>
+    void UpdatePlayerBallImage(GameObject playerBall) {
+        if (playerBall == null) return;
+        //すべてを非表示
+        foreach (var ball in playerBallImages) { 
+            ball.SetActive(false);
+            //ボールが同じ名前の画像があれば、それは表示とする。
+            if (ball.transform.gameObject.name.Equals(playerBall.transform.gameObject.name)) {
+                ball.SetActive(true);
+            }
+        }
+    }
+
+}
